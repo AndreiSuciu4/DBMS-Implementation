9311d22b47e0af1f36d21e76672b2a9c04ec0a1a
Suciu Andrei <andreisuciu146@gmail.com>
2023-10-21 16:35:31 +0300

    First commit

---
+++ src/main/java/com/example/dbmsImplementation/DbmsImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbmsImplementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbmsImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbmsImplementationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dbmsImplementation/DbmsImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbmsImplementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbmsImplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
